768b6e686865eaf911dca3010a586934f8bb1f8f
Ahmad <ahmadzaraei@gmail.com>
2017-10-13 19:07:27 -0700

    Done

---
+++ SocialNetwork/Controllers/SignInViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialNetwork
+//
+//  Created by Ahmad Zaraei on 10/13/17.
+//  Copyright © 2017 Ahmad Zaraei. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
