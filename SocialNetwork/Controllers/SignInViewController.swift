446135c62ff9f95e18be87e2568d688cc5ca56df
Ahmad <ahmadzaraei@gmail.com>
2017-10-14 19:21:38 -0700

    Set the content types for the keyboard, set up regular logging in through e-mail and password

--- SocialNetwork/Controllers/SignInViewController.swift
+++ SocialNetwork/Controllers/SignInViewController.swift
@@ -14,9 +14,30 @@ import FirebaseAuth
 
 class SignInViewController: UIViewController {
 
+  @IBOutlet weak var emailAddressTextField: CustomTextField!
+
+  @IBOutlet weak var passwordTextField: CustomTextField!
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  @IBAction func signInWithCustomAccountAction(_ sender: Any) {
+    if let email = emailAddressTextField.text, let password = passwordTextField.text {
+      Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
+        if let authError = error {
+          print("Unable to login using using e-mail, error is:\(authError)")
+          // TODO(ahmadzaraei): Show an error to the user.
+          return
+        }
+        if let authUser = user {
+          print("User logged in successfully :\(authUser)")
+        }
+      })
+    } else {
+      // TODO(ahmadzaraei): Show a pop-up to the user.
+    }
   }
 
   @IBAction func facebookLoginAction(_ sender: Any) {
