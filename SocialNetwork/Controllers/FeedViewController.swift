27a0fd82d578ec7d7fd0a21a307e380bca445158
Ahmad <ahmadzaraei@gmail.com>
2017-10-14 20:29:47 -0700

    Add signout functionality

--- SocialNetwork/Controllers/FeedViewController.swift
+++ SocialNetwork/Controllers/FeedViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import SwiftKeychainWrapper
+import FirebaseAuth
 
 class FeedViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  @IBAction func signoutAction(_ sender: Any) {
+    // First actually log the user out of Firebase.
+    do {
+      try Auth.auth().signOut()
+    } catch let error as NSError {
+      print("Could not sign user out, error is \(error.localizedDescription)")
+      // TODO(ahmadzaraei): Show user a dialog for this error.
+      return
+    }
+    // Now that we've signed the user out
+    // clear the key chain.
+    let keychainRemovalResult = KeychainWrapper.standard.removeAllKeys()
+    print("Keychain removal result is \(keychainRemovalResult)")
+    // Remove the view controller that was presented
+    // Modally.
+    dismiss(animated: true, completion: nil)
+  }
 
-        // Do any additional setup after loading the view.
+  override func viewDidLoad() {
+        super.viewDidLoad()
     }
 }
